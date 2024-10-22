a57113397c4968307ac3312bd87ab25e409cb16d
kashiarain <kashiarain2000@gmail.com>
2024-10-22 21:57:30 +0500

    Minimal reproduction of NuxtUI3 with Nuxt 4

---
+++ app/app.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<NuxtRouteAnnouncer />
+		<!-- <NuxtWelcome /> -->
+		<UApp>
+			<h1>Hello</h1>
+		</UApp>
+	</div>
+</template>
+<style>
+	@import 'tailwindcss';
+	@import '@nuxt/ui';
+</style>
